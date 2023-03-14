ba06c072f1544f576ca5aa1413a0350022635db9
jiansen <1096129293@qq.com>
2023-03-14 09:43:15 +0000

    s

--- app.js
+++ app.js
@@ -4,28 +4,13 @@ const wss = new WebSocketServer({
 });
 
 
-/**
- * {
-    "userLevelSrc": "https://p6-webcast.douyinpic.com/img/webcast/user_grade_level_v5_38.png~tplv-obj.image",
-    "userLevel": "38",
-    "fansLevelSrc": "https://p6-webcast.douyinpic.com/img/webcast/fansclub_level_v6_10.png~tplv-obj.image",
-    "fansLevel": "10",
-    "fansLightName": "琳琳7",
-    "userNick": "wjunkop",
-    "message": "送出了 粉丝灯牌 × 1",
-    "isGift": true,
-    "giftUrl": "https://p3-webcast.douyinpic.com/img/webcast/722e56b42551d6490e5ebd9521287c67~tplv-obj.png",
-    "giftId": "粉丝灯牌",
-    "giftNum": "1"
-}
- */
 wss.on("connection", function connection(ws) {
     console.log("客户端连接成功");
     ws.on("message", function message(data) {
         let message = JSON.parse(data.toString())
         switch (message.action) {
             case 'message':
-                console.log(`[${new Date().toLocaleTimeString()}] : ${message.userNick}${message.message}`)
+                console.log(`[${new Date().toLocaleTimeString()}] : ${message.message.user_nickName}${message.message.message}`)
                 break
             case 'join':
                 console.log(`[${new Date().toLocaleTimeString()}] : ${message.message} 进入`)
--- client.js
+++ client.js
@@ -62,14 +62,8 @@ function init() {
     chatObserverrom = new MutationObserver(function(mutationsList, observer) {
         for (let mutation of mutationsList) {
             if (mutation.type === 'childList' && mutation.addedNodes.length) {
-                utils.getMessagePly(mutation.addedNodes[0])
-                    // a[Object.keys(a)[1]].children.props.message.payload
-                    // let obj = mutation.addedNodes[0]
-                    // let msg = obj.children[0]
-                    // if (msg) {
-                    //     let mesgobj = utils.messageParse(msg)
-                    //     ws.send(JSON.stringify({ action: 'message', message: mesgobj }));
-                    // }
+                let message = utils.messageParse(mutation.addedNodes[0])
+                ws.send(JSON.stringify({ action: 'message', message: message }));
             }
         }
     });
@@ -80,86 +74,38 @@ function init() {
 var utils = {}
 
 utils.messageParse = function(dom) {
-    let nodes = dom.childNodes
-    const result = {}
-    nodes.forEach((el, index) => {
-        switch (index) {
-            case 0: // 用户信息
-                let userinfo_image = el.querySelectorAll('img')
-                let len = userinfo_image.length // 0 啥也没有 1 没有粉丝灯牌, 2 有粉丝灯牌, 3管理员
-                    // 管理员
-                if (userinfo_image.length === 3) {
-                    userinfo_image[0] = userinfo_image[1]
-                    result.admin = true
-                }
-                // 用户等级
-                if (userinfo_image[0]) {
-                    result.userLevelSrc = userinfo_image[0].src
-                    result.userLevel = utils.getLevel(userinfo_image[0].src)
-                }
-                if (userinfo_image[1]) {
-                    result.fansLevelSrc = userinfo_image[1].src
-                    result.fansLevel = utils.getLevel(userinfo_image[1].src)
-                    result.fansLightName = el.childNodes[0].innerText
-                }
-                break
-            case 1: // 昵称
-                result.userNick = el.childNodes[0].data
-                break
-            case 2: // 内容
-                let clen = el.childNodes.length // 1 礼物
-                let msgitem = el.childNodes[0]
-                result.message = msgitem.outerText
-
-                if (msgitem.children.length) {
-                    let gift = msgitem.querySelector('img')
-                    result.isGift = true
-                    result.giftUrl = gift.src
-
-                    // 将礼物图片转换成名称, 需要补充
-                    result.giftId = utils.getGiftId(gift.src)
-                        // 修改礼物消息 
-                    result.message = result.message.replace('×', result.giftId + ' ×')
-
-                    // 礼物数量
-                    if (gift.nextSibling) {
-                        result.giftNum = gift.nextSibling.childNodes[1].data
-                    }
-                }
-                break
-        }
-    })
+    let msg = dom[propsId].children.props.message.payload
+    let result = {
+        user_nickName: msg.user.nickname,
+        user_id: msg.user.id,
+        user_gender: msg.user.gender === 1 ? '男' : '女',
+        user_level: msg.user.level,
+        user_levelImage: msg.user.badgeImageListList[0].urlListList[0],
+        user_avatar: msg.user.avatarThumb.urlListList[0],
+        user_isAdmin: msg.user.userAttr.isAdmin,
+        user_fansLevel: parseInt(msg.user.badgeImageListV2List[0].content.level),
+        user_fansLightName: msg.user.badgeImageListV2List[0].content.alternativeText,
+    }
+    switch (msg.common.method) {
+        case 'WebcastGiftMessage':
+            result = Object.assign(result, {
+                message: msg.common.describe,
+                isGift: true,
+                gift_id: msg.gift.id,
+                gift_name: msg.gift.name,
+                gift_number: parseInt(msg.groupCount),
+                gift_image: msg.gift.icon.urlListList[0],
+                gift_diamondCount: msg.gift.diamondCount,
+                gift_describe: msg.gift.describe,
+            })
+            break
+        case 'WebcastChatMessage':
+            result = Object.assign(result, {
+                isGift: false,
+                message: msg.content
+            })
+            break
+    }
+    console.log(result)
     return result
-}
-
-utils.getLevel = function(str) {
-    if (!str) return 0
-    return str.substring(str.lastIndexOf('_') + 1, str.indexOf('.png'))
-}
-
-utils.getGiftId = function(str) {
-    if (!str) return ''
-    let id = str.substring(str.lastIndexOf('/') + 1, str.indexOf('~'))
-    return GIFT_IDS_ENUM[id] || '礼物'
-}
-
-utils.getMessagePly = function(m) {
-    let msg = m[propsId].children.props.message.payload
-    console.log(msg)
-    return msg
-}
-
-
-// 礼物哈希值
-const GIFT_IDS_ENUM = {
-    '0ea40b8376ef8157791b928a339ed9c9': '小心心',
-    '898bc8988a737b909e952854747c8f7e': '玫瑰',
-    'a29d6cdc0abb7286fdd403915196eaa7': '鲜花',
-    'bd4ad62dfcc021fda39080166bcbd698': '跑车',
-    'cadd229a47b7fad58ba021c7d4638516': '棒棒糖',
-    'a24b3cc863742fd4bc3de0f53dac4487': '啤酒',
-    'eee04e798ad7f08c9faf577f52e258f9': '墨镜',
-    '722e56b42551d6490e5ebd9521287c67': '粉丝灯牌',
-    '802a21ae29f9fae5abe3693de9f874bd': '抖币',
-    '53b3b0e512c62bd1eb4ea9df9bf38cf7': '520',
 }
