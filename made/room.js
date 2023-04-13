ada338babd919be64b7122cb04ab5920103a0f16
jiansen <1096129293@qq.com>
2023-04-13 16:32:10 +0800

    Create room.js

---
+++ made/room.js
@@ -0,0 +1,135 @@
+const Barrage = class {
+    propsId = null
+    chatDom = null
+    roomJoinDom = null
+    ws = null
+    observer = null
+    chatObserverrom = null
+    option = {}
+    constructor() {
+        this.propsId = Object.keys(document.querySelector('.webcast-chatroom___list'))[1]
+        this.chatDom = document.querySelector('.webcast-chatroom___items').children[0]
+        this.roomJoinDom = document.querySelector('.webcast-chatroom___bottom-message')
+    }
+    runServer() {
+        let _this = this
+        if (this.option.join) {
+            this.observer = new MutationObserver((mutationsList) => {
+                for (let mutation of mutationsList) {
+                    if (mutation.type === 'childList' && mutation.addedNodes.length) {
+                        let dom = mutation.addedNodes[0]
+                        let user = dom[this.propsId].children.props.message.payload.user
+                        let msg = {
+                            ...this.getUser(user),
+                            ... { msg_content: `${user.nickname} 来了` }
+                        }
+                        if (this.eventRegirst.join) {
+                            this.event['join'](msg)
+                        }
+                        insertDom({ action: 'join', message: msg })
+                    }
+                }
+            });
+            this.observer.observe(this.roomJoinDom, { childList: true });
+
+        }
+
+        this.chatObserverrom = new MutationObserver((mutationsList, observer) => {
+            for (let mutation of mutationsList) {
+                if (mutation.type === 'childList' && mutation.addedNodes.length) {
+                    let b = mutation.addedNodes[0]
+                    if (b[this.propsId].children.props.message) {
+                        let message = this.messageParse(b)
+                        if (message) {
+                            if (this.eventRegirst.message) {
+                                this.event['join'](message)
+                            }
+                            if (_this.option.message === false && !message.isGift) {
+                                return
+                            }
+                            insertDom({ action: 'message', message: message })
+                        }
+                    }
+                }
+            }
+        });
+        this.chatObserverrom.observe(this.chatDom, { childList: true });
+    }
+    getUser(user) {
+        if (!user) {
+            return
+        }
+        let msg = {
+            user_level: this.getLevel(user.badgeImageList, 1),
+            user_fansLevel: this.getLevel(user.badgeImageList, 7),
+            user_id: user.id,
+            user_nickName: user.nickname,
+            user_avatar: user.avatarThumb.urlList[0],
+            user_gender: user.gender === 1 ? '男' : '女',
+            user_isAdmin: user.userAttr.isAdmin,
+            user_fansLightName: "",
+            user_levelImage: ""
+        }
+        return msg
+    }
+    getLevel(arr, type) {
+        if (!arr || arr.length === 0) {
+            return 0
+        }
+        let item = arr.find(i => {
+            return i.imageType === type
+        })
+        if (item) {
+            return parseInt(item.content.level)
+        } else {
+            return 0
+        }
+    }
+    messageParse(dom) {
+        if (!dom[this.propsId].children.props.message) {
+            return null
+        }
+        let msg = dom[this.propsId].children.props.message.payload
+        let result = {
+            gift_id: null,
+            gift_name: null,
+            gift_number: null,
+            gift_image: null,
+            gift_diamondCount: null,
+            gift_describe: null,
+        }
+        result = Object.assign(result, this.getUser(msg.user))
+        switch (msg.common.method) {
+            case 'WebcastGiftMessage':
+                result = Object.assign(result, {
+                    msg_content: msg.common.describe,
+                    isGift: true,
+                    gift_id: msg.gift.id,
+                    gift_name: msg.gift.name,
+                    gift_number: parseInt(msg.comboCount),
+                    gift_image: msg.gift.icon.urlListList[0],
+                    gift_diamondCount: msg.gift.diamondCount,
+                    gift_describe: msg.gift.describe,
+                })
+                break
+            case 'WebcastChatMessage':
+                result = Object.assign(result, {
+                    isGift: false,
+                    msg_content: msg.content
+                })
+                break
+            default:
+                result = Object.assign(result, {
+                    isGift: false,
+                    msg_content: msg.content
+                })
+                break
+        }
+        return result
+    }
+}
+document.querySelector('.basicPlayer').remove()
+
+function insertDom(msg) {
+    console.log(msg)
+}
